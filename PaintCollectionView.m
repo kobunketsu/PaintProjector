a3bd3e3a76f2a7807c384bbeec69ba3178cc3b81
kobunketsu <kobunketsu0928@hotmail.com>
2014-01-22 09:51:01 +0800

    new a branch for test collection view

---
+++ PaintCollectionView.m
@@ -0,0 +1,31 @@
+//
+//  PaintCollectionView.m
+//  PaintProjector
+//
+//  Created by 胡 文杰 on 1/22/14.
+//  Copyright (c) 2014 WenjiHu. All rights reserved.
+//
+
+#import "PaintCollectionView.h"
+
+@implementation PaintCollectionView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
---
+++ PaintCollectionViewCell.m
@@ -0,0 +1,31 @@
+//
+//  PaintCollectionViewCell.m
+//  PaintProjector
+//
+//  Created by 胡 文杰 on 1/22/14.
+//  Copyright (c) 2014 WenjiHu. All rights reserved.
+//
+
+#import "PaintCollectionViewCell.h"
+
+@implementation PaintCollectionViewCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
