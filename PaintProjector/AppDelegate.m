bac6c66a8fd12f4e92f40ad4edb2ec8287592e20
kobunketsu <kobunketsu0928@hotmail.com>
2014-05-28 00:26:52 +0800

    add Flurry framework

--- PaintProjector/AppDelegate.m
+++ PaintProjector/AppDelegate.m
@@ -9,9 +9,7 @@
 #import "AppDelegate.h"
 
 #import <DBChooser/DBChooser.h>
-#import <Crashlytics/Crashlytics.h>
 #import "AnaDrawIAPManager.h"
-#import "TestFlight.h"
 #import "BBXBeeblex.h"
 #import "iRate.h"
 
@@ -35,6 +33,9 @@
     DebugLogSystem(@"applicationDidFinishLaunchingWithOptions");
     
     [BBXBeeblex initializeWithAPIKey:@"OWEwODU3ZWVjNzU3NmNkMjg2MDFiODg1ODE3NThjNTk2MTliYTE3ZDM5YTA2MDcwODI4YWRjN2FiMzgwNzAwMDc1YmI1ZDFkMTE0Njk2YzBhMDNkYzhhNTY1M2NiY2I1NGFjM2U2MDMyMzYxYTcyYjU5ZTRmNzBjMjU3ZWYzNWUsLS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlJQklqQU5CZ2txaGtpRzl3MEJBUUVGQUFPQ0FROEFNSUlCQ2dLQ0FRRUE1cW5mN0hka29UZXRVeEYwOHM2Qwp2ME95NEtOd3F4bGVvcW5wd3pUa1orUXVQTWhkU0xXQ1NSUHhOUjNRYjlzeVEvVHhkS1hWTUt2VjJicldWZE42ClhnNUxQT2NjZ0l6ZWFQQWp2UXdjcGc2KzZmOFlKTFpEUGtudzAxeGhZZkRCajJqNzY3NFNRVmNVenB1QUZqYVEKeEV2Zm5DUkxIZUh5SnNWTm1YZGp1clZJOW9xTERVcnk5S3hEdkVXMENyKzBqK0NaYzlkMytxOXBBRXdVRUxlaApSY3g2cTdjMmxtL0c1NU9LTkw0NmVqcTgrK3VTTEJSL0pkWUF5MFR2MnY4bkdHQURhb2tjUG9XMUVVdjk1Z0gyCnBqTC8vSzNDY1dpZUo0U2d0NVRnZFBPNmhhS3NaTk5lZDVxNmZ6Vm9LWlB6WVdPcnk4dGluclNGSFZtckt1TTMKeXdJREFRQUIKLS0tLS1FTkQgUFVCTElDIEtFWS0tLS0t"];
+    
+    [Flurry setCrashReportingEnabled:NO];
+    [Flurry startSession:@"M8DMDS5GW352GZQ4TN3B"];
     
     [TestFlight takeOff:@"d99fa9e3-3923-4f55-acea-0a1e077b133f"];
     
